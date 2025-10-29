ad1b5c005b8513a874beea402b166a85f63ceae8
rofehr <roland@diefehrs.de>
2025-10-29 13:52:21 +0100

    sS

---
+++ plugin-gstreamer.cpp
@@ -0,0 +1,37 @@
+#include <vdr/plugin.h>
+#include "gstdevice.h"
+#include <fstream>
+#include <iostream>
+
+class cPluginGStreamer : public cPlugin {
+private:
+  cGstDevice *device_ = nullptr;
+  std::string pipeline_desc_;
+  void LoadPipelineConfig() {
+    std::ifstream conf("/etc/vdr/plugins/gstreamer.conf");
+    if (conf.is_open()) {
+      std::getline(conf, pipeline_desc_);
+      conf.close();
+    }
+  }
+public:
+  cPluginGStreamer() {}
+  virtual ~cPluginGStreamer() {}
+  const char *Version() { return "0.0.3"; }
+  const char *Description() { return "GStreamer output plugin (AV)"; }
+  bool ProcessArgs(int argc, char *argv[]) override {
+    for (int i = 0; i < argc; ++i) {
+      if (!strncmp(argv[i], "pipeline=", 9)) pipeline_desc_ = argv[i] + 9;
+    }
+    return true;
+  }
+  bool Start() override {
+    if (pipeline_desc_.empty()) LoadPipelineConfig();
+    device_ = new cGstDevice(pipeline_desc_);
+    device_->SetEstimatedFps(25.0);
+    device_->SetEstimatedAudioSampleRate(48000);
+    return true;
+  }
+  void Stop() override { delete device_; device_ = nullptr; }
+};
+VDRPLUGINCREATOR(cPluginGStreamer);
